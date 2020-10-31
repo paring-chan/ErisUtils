f393ef2faeeae9e13b2e2e68bf44a27df41f5e39
pikokr <parangee9706@gmail.com>
2020-10-31 09:31:05 +0900

    Feat: Enable declaration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import ErisUtilClient from "./structs/ErisUtilClient";
-
-new ErisUtilClient('')
+export {default as Listener} from './structs/listener/Listener'
+export {default as ListenerHandler} from './structs/listener/ListenerHandler'
+export {default as ErisUtilClient} from './structs/ErisUtilClient'
